0bcb4db596afb2d04ad9c0ddf02e7184b0013e99
WyldCode <uj.wyldstyle@gmail.com>
2026-09-07 02:04:48 +0530

    JP_01

---
+++ Powershell/01_test.ps1
@@ -0,0 +1,15 @@
+# Get-Service
+# Get-Date
+    # now ther's no way to remember all Verb-Noun commandsLets so
+# Get-Command
+    # now it gives all the commands that are loaded
+    # but let's say you remeber the noun but not the verb, then 
+# Get-Command -Noun Service
+# Get-Command -Verb Get
+
+# Get-Help Get-Service -FulL
+# Get-Alias
+
+# cd == Set-Location -Path C:\Users\Public\Programming\PowerShell
+
+Get-ExecutionPolicy
