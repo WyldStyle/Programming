6f35baa1c11452415f6cf8c381a15bc513948145
WyldCode <uj.wyldstyle@gmail.com>
2026-09-09 08:34:42 +0530

    JP_05

--- Powershell/05_PipeLine.ps1
+++ Powershell/05_PipeLine.ps1
@@ -8,3 +8,13 @@ Start-Service -Name $serviceName
 # Get-Help stop-service -Full
 
 Get-Service -Name $serviceName | Stop-Service
+
+$serviceName | Get-service | Start-Service
+Get-Service -Name $serviceName
+
+$serviceName = New-Object -TypeName System.Collections.ArrayList
+$serviceName.AddRange(@('spooler','w32Time'))
+$serviceName | Get-Service
+$serviceName | Get-Service | Start-Service
+
+# $serviceName | Get-Service | ForEach-Object{Write-Object "Service : $($_.DisplayName) is currently $($_.Status)"}
